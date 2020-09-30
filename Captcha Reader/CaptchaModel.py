d39272f787e92566e9c6c41d437f47e140937a9d
Shivam Raj <shivamrajsharma86@gmail.com>
2020-09-30 21:46:35 +0530

    Update CaptchaModel.py

--- Captcha Reader/CaptchaModel.py
+++ Captcha Reader/CaptchaModel.py
@@ -2,16 +2,16 @@ import  torch
 import torch.nn as nn
 
 class ConvBlock(nn.Module):
-    def __init__(self, input_channels, out_channels, dropout):
+    def __init__(self, input_channels, out_channels, dropout, max_pool_size):
         super(ConvBlock, self).__init__()
         self.conv1 = nn.Conv2d(input_channels, out_channels, kernel_size=3, padding=1)
-        self.batch_norm = nn.BatchNorm2d(out_channels)
         self.dropout = nn.Dropout(dropout)
+        self.max_pool = nn.MaxPool2d(kernel_size=max_pool_size)
     
     def forward(self, x):
         x = self.conv1(x)
-        x =  self.batch_norm(x)
         x = self.dropout(nn.LeakyReLU()(x))
+        x = self.max_pool(x)
         return x
 
 class CNN(nn.Module):
@@ -21,24 +21,26 @@ class CNN(nn.Module):
         out_channels, 
         kernel_size, 
         dropout,
-        num_conv_layers
+        num_conv_layers,
+        max_pool_size
     ):
         super(CNN, self).__init__()
         self.conv = nn.Conv2d(input_channels, out_channels, kernel_size=kernel_size, padding=1)
         self.Lrelu = nn.LeakyReLU()
-        self.batch_norm = nn.BatchNorm2d(out_channels)
         self.dropout = nn.Dropout(dropout)
+        self.max_pool = nn.MaxPool2d(kernel_size=max_pool_size)
         self.layers = nn.Sequential(
             *[
-                ConvBlock(out_channels, out_channels, dropout)
+                ConvBlock(out_channels, out_channels, dropout, max_pool_size)
                 for _ in range(num_conv_layers)
             ]
         )
     
     def forward(self, x):
         x = self.conv(x)
-        x = self.Lrelu(self.batch_norm(x))
+        x = self.Lrelu(x)
         x = self.dropout(x)
+        x = self.max_pool(x)
         x = self.layers(x)
         return x
 
@@ -72,6 +74,7 @@ class CaptchaModel(nn.Module):
         out_channels, 
         kernel_size, 
         conv_dropout,
+        max_pool_size,
         num_conv_layers,
         input_dims, 
         hidden_dims,
@@ -85,7 +88,8 @@ class CaptchaModel(nn.Module):
             out_channels, 
             kernel_size, 
             conv_dropout,
-            num_conv_layers
+            num_conv_layers,
+            max_pool_size
         )
         
         self.rnn = RNN(
@@ -98,7 +102,7 @@ class CaptchaModel(nn.Module):
 
     def forward(self, x):
         x = self.cnn(x)
-        x = x.view(-1, x.shape[1]*x.shape[2], x.shape[3])
+        x = x.reshape(-1, x.shape[1]*x.shape[2], x.shape[3])
         x = x.permute(0, 2, 1)
         x = self.rnn(x)
         return x
