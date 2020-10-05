dea3962725820441e19212ecc0fbecac1c18d53e
Shivam Raj <shivamrajsharma86@gmail.com>
2020-10-06 02:53:43 +0530

    Update CONFIG.py

--- BERT Sentiment Analysis/src/CONFIG.py
+++ BERT Sentiment Analysis/src/CONFIG.py
@@ -3,17 +3,9 @@ import transformers
 max_len = 200
 Epochs = 2
 Batch_Size = 8
-BERT_Path = 'input/'
+
+BERT_Path = '../input/'
 Model_Path= '../model/bert_sentiment.bin'
 Tokenizer = transformers.BertTokenizer.from_pretrained(BERT_Path, do_lower_case=True)
 
-# sentence = '[PAD]'
-
-# inputs = Tokenizer.encode(sentence)
-# print(inputs)
-
-# ids = inputs['ids']
-# mask = inputs['mask']
-# token_type_ids = inputs['token_type_ids']
-# print(ids)
-
+pad_idx = Tokenizer.encode('[PAD]')[1]
