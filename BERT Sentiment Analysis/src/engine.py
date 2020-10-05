ae4f64d2dc520029d160203e814b0df66cb3839e
Shivam Raj <shivamrajsharma86@gmail.com>
2020-10-06 02:55:20 +0530

    Update engine.py

--- BERT Sentiment Analysis/src/engine.py
+++ BERT Sentiment Analysis/src/engine.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch 
 import torch.nn as nn
 from tqdm import tqdm
@@ -5,9 +6,9 @@ from tqdm import tqdm
 def loss_fn(output, target):
     return nn.BCEWithLogitsLoss()(output, target)
 
-def acc(output, target):
+def accuracy(output, target):
     output = (torch.sigmoid(output) > 0.5)*1
-    acc = torch.sum(output==target)/target.shape[0]
+    acc = torch.mean((output == target)*1.0)
     return acc
 
 
@@ -16,10 +17,10 @@ def train(model, dataloader, optimizer, scheduler, device):
     running_loss = 0
     running_acc = 0
     for num_steps, data in tqdm(enumerate(dataloader), total=len(dataloader)):
-        ids = data['ids']
-        mask = data['mask']
-        token_type_ids = data['token_type_ids']
-        target = data['sentiment']
+        ids = data['ids'].to(device)
+        mask = data['mask'].to(device)
+        token_type_ids = data['token_type_ids'].to(device)
+        target = data['sentiment'].to(device)
 
         output = model(ids, mask, token_type_ids)
 
@@ -27,7 +28,7 @@ def train(model, dataloader, optimizer, scheduler, device):
 
         loss.backward()
         running_loss += loss.item()
-        running_acc += acc(output, target.reshape(-1, 1))
+        running_acc += accuracy(output, target.reshape(-1, 1)).item()
 
         optimizer.step()
         scheduler.step()
@@ -46,24 +47,16 @@ def eval(model, dataloader, device):
     running_loss = 0
     with torch.no_grad():
         for num_steps, data in tqdm(enumerate(dataloader), total=len(dataloader)):
-            ids = data['ids']
-            mask = data['mask']
-            token_type_ids = data['token_type_ids']
-            target = data['sentiment']
-
-            ids = ids.to(device)
-            mask = mask.to(device)
-            token_type_ids = token_type_ids.tp(device)
+            ids = data['ids'].to(device)
+            mask = data['mask'].to(device)
+            token_type_ids = data['token_type_ids'].to(device)
+            target = data['sentiment'].to(device)
 
             output = model(ids, mask, token_type_ids)
 
-            running_loss += loss_fn(output, target.reshape(-1, 1))
-            running_acc = + acc(output, target.reshape(-1, 1))
+            running_loss += loss_fn(output, target.reshape(-1, 1)).item()
+            running_acc +=  accuracy(output, target.reshape(-1, 1)).item()
     epoch_loss = running_loss/len(dataloader)
-    epoch_acc = running_acc/len(dataloader)
+    epoch_acc = running_acc*100/len(dataloader)
     
     return [epoch_acc, epoch_loss]
-        
-
-
-    
