d3647b0df70d7119a80a08f1fd33d19c44ba11be
Shivam Raj <shivamrajsharma86@gmail.com>
2020-10-06 02:45:58 +0530

    Update engine.py

--- NER/src/engine.py
+++ NER/src/engine.py
@@ -2,60 +2,80 @@ import torch
 import torch.nn as nn
 from tqdm import tqdm 
 
-def loss_fn(ouput, target):
-    return  nn.CrossEntropyLoss()(ouput, target)
+def accuracy(output, target):
+    predicted = torch.argmax(torch.softmax(output, dim=-1), dim=-1)
+    predicted = predicted.reshape(-1)
+    target = target.reshape(-1)
+    acc = torch.mean((predicted == target)*1.0)
+    return acc
 
-def train(model, dataloader, optimizer, scheduler, device):
+
+def loss_fn(output, target):
+    return  nn.CrossEntropyLoss()(output, target)
+
+def train(model, dataloader, optimizer, device):
     model.train()
     running_loss = 0
+    running_pos_acc = 0
+    running_tag_acc = 0
     
     for num_steps, data in tqdm(enumerate(dataloader), total=len(dataloader)):
         for p in model.parameters():
             p.grad = None
-
-        sentence = data['sentence'].to(device)
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
         sentence_idx = data['sentence_idx'].to(device)
-        pos_tag = data['pos_tag'].to(device)
-        ner_tag = data['ner_tag'].to(device)
+        pos = data['pos'].to(device)
+        tag = data['tag'].to(device)
 
-        pos_output, ner_output = model(sentence_idx)
+        pos_output, tag_output = model(sentence_idx)
         
-        loss_pos = loss_fn(pos_output.reshape(-1, pos_output.shape[-1]), pos_tag.reshape(-1, 1))
-        loss_ner = loss_fn(ner_output.reshape(-1, ner_output.shape[-1]), ner_tag.reshape(-1, 1))
+        pos_loss = loss_fn(pos_output.reshape(-1, pos_output.shape[-1]), pos.reshape(-1))
+        tag_loss = loss_fn(tag_output.reshape(-1, tag_output.shape[-1]), tag.reshape(-1))
 
-        loss = (loss_pos + loss_ner)/2
+        running_pos_acc += accuracy(pos_output, pos).item()
+        running_tag_acc += accuracy(tag_output, tag).item()
 
+
+        loss = (pos_loss + tag_loss)/2
         running_loss += loss.item()
 
         loss.backward()
         optimizer.step()
-        scheduler.step()
     
     epoch_loss = running_loss/len(dataloader)
-    return epoch_loss
+    pos_acc = running_pos_acc/len(dataloader)
+    tag_acc = running_tag_acc/len(dataloader)
+    
+    return epoch_loss, pos_acc, tag_acc
 
 
 
 def eval(model, dataloader, device):
     running_loss = 0
+    running_pos_acc = 0
+    running_tag_acc = 0
     model.eval()  
     with torch.no_grad():
         for num_steps, data in tqdm(enumerate(dataloader), total=len(dataloader)):
-            sentence = data['sentence'].to(device)
             sentence_idx = data['sentence_idx'].to(device)
-            pos_tag = data['pos_tag'].to(device)
-            ner_tag = data['ner_tag'].to(device)
+            pos = data['pos'].to(device)
+            tag = data['tag'].to(device)
 
-            pos_output, ner_output = model(sentence_idx)
+            pos_output, tag_output = model(sentence_idx)
             
-            loss_pos = loss_fn(pos_output.reshape(-1, pos_output.shape[-1]), pos_tag.reshape(-1, 1))
-            loss_ner = loss_fn(ner_output.reshape(-1, ner_output.shape[-1]), ner_tag.reshape(-1, 1))
+            pos_loss = loss_fn(pos_output.reshape(-1, pos_output.shape[-1]), pos.reshape(-1))
+            tag_loss = loss_fn(tag_output.reshape(-1, tag_output.shape[-1]), tag.reshape(-1))
 
-            loss = (loss_pos + loss_ner)/2
+            running_pos_acc += accuracy(pos_output, pos).item()
+            running_tag_acc += accuracy(tag_output, tag).item()
+
+            loss = (pos_loss + tag_loss)/2
 
             running_loss += loss.item()
     epoch_loss = running_loss/len(dataloader)
+    pos_acc = running_pos_acc/len(dataloader)
+    tag_acc = running_tag_acc/len(dataloader)
 
-    return epoch_loss
+    return epoch_loss, pos_acc, tag_acc
 
         
