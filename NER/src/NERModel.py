8288de2da3787c6286697d2377e528015bf9ed84
Shivam Raj <shivamrajsharma86@gmail.com>
2020-10-06 02:50:27 +0530

    Update NERModel.py

--- NER/src/NERModel.py
+++ NER/src/NERModel.py
@@ -11,28 +11,30 @@ class LSTMModel(nn.Module):
         hidden_dims,
         num_layers,
         dropout,
+        bidirectional,
         num_pos_class,
-        num_ner_class
+        num_tag_class
     ):
+        super(LSTMModel, self).__init__()
         self.embedding = nn.Embedding(vocab_size, embed_dims)
-        self.rnn = nn.LSTM(
+        self.rnn = nn.GRU(
             embed_dims,
             hidden_dims,
             num_layers=num_layers,
             batch_first=True,
             dropout=dropout,
-            bidirectional=True
+            bidirectional=bidirectional
         )
         self.dropout = nn.Dropout(dropout)
-        self.pos_out = nn.Linear(hidden_dims, num_pos_class)
-        self.ner_out = nn.Linear(hidden_dims, num_ner_class)
+        self.hidden_out = hidden_dims*2 if bidirectional else hidden_dims
+        self.pos_out = nn.Linear(self.hidden_out, num_pos_class)
+        self.tag_out = nn.Linear(self.hidden_out, num_tag_class)
 
-    def forward(self, sentence):
+    def forward(self, x):
         x = self.dropout(self.embedding(x))
         x, _ = self.rnn(x)
         x = self.dropout(x)
         pos_out = self.pos_out(x)
-        ner_out = self.ner_out(x)
+        tag_out = self.tag_out(x)
         
-        return pos_out, ner_out
-    
+        return pos_out, tag_out
